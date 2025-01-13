dff27825b649b7deede9ef8fb6a3a42471c99fd9
jesus ventura <jesus.ventura@saud.gob.sv>
2025-01-12 23:10:50 -0600

    Change title swagger

--- core/swagger.ts
+++ core/swagger.ts
@@ -6,9 +6,9 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'My Express.js API',
+            title: 'People Management API',
             version: '1.0.0',
-            description: 'A sample Express.js API built with TypeScript and Swagger',
+            description: 'A RESTful API for managing people data, with endpoints to add, update, sort, and filter records. Built with Express.js, TypeScript, and Swagger.',
         },
         /*servers: [
             {
